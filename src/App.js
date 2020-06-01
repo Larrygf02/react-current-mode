a8fd0a391c161a7f39bfdf4d1279c7a30f36ba51
larrygf02 <raulgf_02@hotmail.com>
2020-05-31 19:27:24 -0500

    Implementing react current mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
+import { createResourse } from './PersonApi';
+import { Person } from './Person';
 
+const resource = createResourse();
 function App() {
   return (
     <div className="App">
       <h1>Hello!!</h1>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Person resource={resource}/>
+      </Suspense>
     </div>
   );
 }
